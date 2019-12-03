1c3df74ee1254a0b3d9fa932a91f61e96a2d208a
Andreas Schröpfer <andreas@apxp.info>
2019-12-03 16:02:21 +0100

    configuration parser created

--- target.go
+++ target.go
@@ -14,7 +14,7 @@ type target struct {
 func useTarget(t target, fpath string) bool {
 	ext := filepath.Ext(fpath)
 	for _, e := range t.exts {
-		if e == ext || e == "*" {
+		if e == ext || e == ".*" {
 			return true
 		}
 	}
